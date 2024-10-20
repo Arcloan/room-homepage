f1ea613c2331ad38d7929d16ef60142580d4a82a
Arcloan <95delchris@gmail.com>
2024-10-20 22:39:39 +0200

    final commit

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const desktopImages = ["image/desktop-image-hero-1.jpg",
+const desktopImages = ["images/desktop-image-hero-1.jpg",
     "images/desktop-image-hero-2.jpg",
     "images/desktop-image-hero-3.jpg"
 ];
@@ -8,20 +8,22 @@ const mobileImages = ["images/mobile-image-hero-1.jpg",
     "images/mobile-image-hero-3.jpg"
 ];
 
-const mobileSlider = document.querySelector(".c-slider-mobile");
-const hamburger = document.querySelector(".menu > img:first-child");
+const descriptions = document.querySelectorAll(".c-company-description");
+const selectedDescription = ["first", "second", "third"];
 
+const hamburger = document.querySelector(".menu > img:first-child");
 hamburger.addEventListener("click", (e) => {
     const nav = document.querySelector(".primary-navigation-mobile");
     nav.classList.remove("hidden");
 })
 
 const closeIcon = document.querySelector(".primary-navigation-mobile img");
-
 closeIcon.addEventListener("click", (e) => {
     const nav = document.querySelector(".primary-navigation-mobile");
     nav.classList.add("hidden");
 })
+
+const mobileSlider = document.querySelector(".c-slider-mobile");
 
 const mobileControls = {
     currIndex: 1,
@@ -35,6 +37,15 @@ const mobileControls = {
             this.currIndex++;
         }
         let url = mobileImages[this.currIndex - 1];
+        let selectedDescr = selectedDescription[this.currIndex - 1];
+        descriptions.forEach((el) => {
+            el.classList.add("fade-out");
+        });
+        descriptions.forEach((el) => {
+            if (el.classList.contains(selectedDescr)) {
+                el.classList.remove("fade-out");
+            }
+        });
         this.slider.style.backgroundImage = `url("${url}")`;
     },
 
@@ -46,6 +57,15 @@ const mobileControls = {
             this.currIndex--;
         }
         let url = mobileImages[this.currIndex - 1];
+        let selectedDescr = selectedDescription[this.currIndex - 1];
+        descriptions.forEach((el) => {
+            el.classList.add("fade-out")
+        });
+        descriptions.forEach((el) => {
+            if (el.classList.contains(selectedDescr)) {
+                el.classList.remove("fade-out");
+            }
+        });
         this.slider.style.backgroundImage = `url("${url}")`;
     }
 
@@ -64,5 +84,71 @@ mobileArrows.addEventListener("click", (e) => {
     }
     else {
         mobileControls.showPrev();
+    }
+})
+
+
+
+const desktopSlider = document.querySelector(".c-slider-desktop");
+
+const desktopControls = {
+    currIndex: 1,
+    slider: desktopSlider,
+
+    showNext() {
+        if (this.currIndex == 3) {
+            this.currIndex = 1;
+        }
+        else {
+            this.currIndex++;
+        }
+        let url = desktopImages[this.currIndex - 1];
+        let selectedDescr = selectedDescription[this.currIndex - 1];
+        descriptions.forEach((el) => {
+            el.classList.add("fade-out");
+        });
+        descriptions.forEach((el) => {
+            if (el.classList.contains(selectedDescr)) {
+                el.classList.remove("fade-out");
+            }
+        });
+        this.slider.style.backgroundImage = `url("${url}")`;
+    },
+
+    showPrev() {
+        if (this.currIndex == 1) {
+            this.currIndex = 3;
+        }
+        else {
+            this.currIndex--;
+        }
+        let url = desktopImages[this.currIndex - 1];
+        let selectedDescr = selectedDescription[this.currIndex - 1];
+        descriptions.forEach((el) => {
+            el.classList.add("fade-out")
+        });
+        descriptions.forEach((el) => {
+            if (el.classList.contains(selectedDescr)) {
+                el.classList.remove("fade-out");
+            }
+        });
+        this.slider.style.backgroundImage = `url("${url}")`;
+    }
+
+
+}
+
+const desktopArrows = document.querySelector(".arrows-desktop");
+
+desktopArrows.addEventListener("click", (e) => {
+    let selected = e.target.closest(".prev") || e.target.closest(".next");
+    if (! selected) {
+        return;
+    }
+    if (selected.classList.contains("next")) {
+        desktopControls.showNext();
+    }
+    else {
+        desktopControls.showPrev();
     }
 })
